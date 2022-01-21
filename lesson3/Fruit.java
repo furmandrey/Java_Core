1e8007ff08e44ec4a089143af87d479d8ec06866
Furmandrey <furmandrey@yandex.ru>
2022-01-21 13:10:08 +0300

    homeWork-3

---
+++ lesson3/Fruit.java
@@ -0,0 +1,22 @@
+package ru.geekbrains.lesson3;
+
+public class Fruit {
+    protected float weight;
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public float getWeightF() {
+        return weight;
+    }
+}
+
